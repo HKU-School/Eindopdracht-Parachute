e78d4fe8a511212c611a978b76f221fcd3ec4750
Samantha <samanthavanderheijden9910@gmail.com>
2025-10-02 16:37:09 +0200

    Input sysytem
    it will take longer cause of no video alloud

--- Parachute/Assets/Scripts/PlayerScript.cs
+++ Parachute/Assets/Scripts/PlayerScript.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
 public class PlayerScript : MonoBehaviour
 {
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
