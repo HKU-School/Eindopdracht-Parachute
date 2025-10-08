26eefd395a2ba6995c023fd956840d31bf3f6c60
Samantha <samanthavanderheijden9910@gmail.com>
2025-10-08 16:37:33 +0200

    Input system start
    input system goed gezet voor player en script aan gemaakt en alvast  debug shit in gezet om te testen als controller

--- Parachute/Assets/Scripts/PlayerScript.cs
+++ Parachute/Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,30 @@
+using System.Diagnostics;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug; // IDK but if I don't have this I can't do debug.log anymore
 
 public class PlayerScript : MonoBehaviour
 {
-
+    [SerializeField] private float _playerSpeed = 3f;
+    private Rigidbody2D _rb;
+    private float _horizonMove;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        // Checke why horizonMove
+        Debug.Log("Move is pressed");
+    }
+    public void Pause(InputAction.CallbackContext context)
+    {
+        // Action for pause game add later. 
+        Debug.Log("Pause is pressed");
     }
 
     // Update is called once per frame
