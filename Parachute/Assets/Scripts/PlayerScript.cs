2c757d7c3dc14099730d0639194c4a96db8f8c3f
Samantha <samanthavanderheijden9910@gmail.com>
2026-02-27 16:16:07 +0100

    input actions
    Have a problem with the input system will try to start new project if work then will work from that one futher

--- Parachute/Assets/Scripts/PlayerScript.cs
+++ Parachute/Assets/Scripts/PlayerScript.cs
@@ -1,49 +1,50 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
+//[RequireComponent(typeof(PlayerInput))]
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 3f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //    [SerializeField] private float playerSpeed = 3f;
+    //    private Rigidbody2D rb;
+    //    private Vector2 moveInput;
 
-    public void Move(InputAction.CallbackContext context)
-    {
-        // Movement 
-        Debug.Log("Move is pressed");
-        moveInput = context.ReadValue<Vector2>();
-    }
+    //    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //    void Start()
+    //    {
+    //        rb = GetComponent<Rigidbody2D>();
+    //    }
 
-    public void Pause(InputAction.CallbackContext context)
-    {
-        // Action for pause game 
-        Debug.Log("Pause is pressed");
-        GameManager.instance.PauseGame();
-    }
+    //    public void Move(InputAction.CallbackContext context)
+    //    {
+    //        // Movement 
+    //        Debug.Log("Move is pressed");
+    //        moveInput = context.ReadValue<Vector2>();
+    //    }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(moveInput.x * playerSpeed, rb.linearVelocity.y);
-    }
+    //    public void Pause(InputAction.CallbackContext context)
+    //    {
+    //        // Action for pause game 
+    //        Debug.Log("Pause is pressed");
+    //        GameManager.instance.PauseGame();
+    //    }
 
-    // Checking for points. 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Parachutisten"))
-        {
-            GameManager.instance.AddPoint();
-        }
-        if (other.CompareTag("Dog"))
-        {
-            GameManager.instance.DogPoint();
-        }
-    }
+    //    // Update is called once per frame
+    //    void FixedUpdate()
+    //    {
+    //        rb.linearVelocity = new Vector2(moveInput.x * playerSpeed, rb.linearVelocity.y);
+    //    }
+
+    //    // Checking for points. 
+    //    private void OnTriggerEnter2D(Collider2D other)
+    //    {
+    //        if (other.CompareTag("Parachutisten"))
+    //        {
+    //            GameManager.instance.AddPoint();
+    //        }
+    //        if (other.CompareTag("Dog"))
+    //        {
+    //            GameManager.instance.DogPoint();
+    //        }
+    //    }
 }
